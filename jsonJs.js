43cc73da83c2611397c781ff1f802034ac17e74d
Harshita <harshita4420@gmail.com>
2023-07-16 01:49:01 +0530

    updated parse json in js

--- jsonJs.js
+++ jsonJs.js
@@ -5,6 +5,18 @@ myJson = `{
     "score": 20.95,
     "isAdmin": false,
     "license": null,
-    "shopItem": ["food", "clothes", "cosmetics"]
+    "shopItem": ["food", "clothes", "cosmetics"],
+    "myObj": {
+        "name": "nestedjson",
+        "marks": 25,
+        "shopItem2": ["food", "clothes", {"a" : true}] 
+    }
 }`;
 JSON.parse(myJson);
+parsed = JSON.parse(myJson);
+parsed["isAdmin"]
+parsed["shopItem"][2]
+//for nested obj
+parsed = JSON.parse(myJson);
+parsed["myObj"]["shopItem2"]
+parsed["myObj"]["shopItem2"][2]["a"]
