ee9d69ae199638f199a86bad95d9278b1b48c7da
Harshita <harshita4420@gmail.com>
2023-07-16 01:32:29 +0530

    parse json in js

---
+++ jsonJs.js
@@ -0,0 +1,10 @@
+//JS code try in the console in inspect to parse Json data
+
+myJson = `{
+    "name": "Harshita",
+    "score": 20.95,
+    "isAdmin": false,
+    "license": null,
+    "shopItem": ["food", "clothes", "cosmetics"]
+}`;
+JSON.parse(myJson);
